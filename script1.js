bd87fca25354fd642e5c2df49a24ff065e7d59b9
michael-pixel-gif <m.rodriguez59313@gmail.com>
2025-02-28 17:32:21 -0700

    edit

--- script1.js
+++ script1.js
@@ -15,22 +15,44 @@ function slot3(){
 }
 
 function spin(){
+    let number = Number(document.getElementById('gamble').value);
+    
+    if(number >= 1001 || number === '' || number <= 0 ){
+        window.alert("Your bet has to between 1 and 1000");
+        return;
+    }
+
     spinCount++;
     console.log(spinCount)
     document.getElementById('button').style.display = 'block';
     document.getElementById('playButton').style.display = 'none';
-    let slot_1 = slot1();
-    document.getElementById("slot1").innerHTML = slot_1;
-    let slot_2 = slot2();
-    document.getElementById("slot2").innerHTML = slot_2;
-    let slot_3 = slot3();
-    document.getElementById("slot3").innerHTML = slot_3;
-    console.log(slot_1, slot_2, slot_3);
 
+    let intervalCount = 0;
+
+    let interval = setInterval(function(){
+        intervalCount++;
+        if(intervalCount >= 6){
+            clearInterval(interval)
+        }else{
+            let slot_1 = slot1();
+            document.getElementById("slot1").innerHTML = slot_1;
+            let slot_2 = slot2();
+            document.getElementById("slot2").innerHTML = slot_2;
+            let slot_3 = slot3();
+            document.getElementById("slot3").innerHTML = slot_3;
+            console.log(slot_1, slot_2, slot_3);
+        }
+
+    }, 100)
+
+   
+    
     const outcome = document.getElementById("outcome");
     const money = document.getElementById("money");
     const moneyMsg = document.getElementById("moneyMsg");
-    moneyMsg.style.display = 'none'
+    moneyMsg.style.display = 'none';
+
+    
 
     if(spinCount % 5 == 0){
         startingCash += 200;
@@ -41,13 +63,13 @@ function spin(){
 
     if(slot_1 == slot_2 && slot_1 == slot_3){
         let win = document.getElementById('win');
-        startingCash += 200;
+        startingCash += number;
         outcome.innerHTML = `You Won!`;
         money.innerHTML = `${startingCash}`;
         win.play();
         
     } else {
-        startingCash -= 100;
+        startingCash -= number;
         let lose = document.getElementById('lose');
         outcome.innerHTML = `You Lost!`;
         money.innerHTML = `${startingCash}`;
@@ -59,6 +81,7 @@ function spin(){
         spinCount = 0;
         return playAgain();
     }
+
 };
 document.getElementById("money").innerHTML = `${startingCash}`;
 
