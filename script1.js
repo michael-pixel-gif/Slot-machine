6e7dd2f3635b6d015f71041b8f02f4928e520c00
michael-pixel-gif <m.rodriguez59313@gmail.com>
2025-03-03 17:25:51 -0700

    yes

--- script1.js
+++ script1.js
@@ -2,38 +2,32 @@ let startingCash = 1000;
 let spinCount = 0;
 const faces = ['🐲','🌶️','🔥'];
 
+document.getElementById("money").innerHTML = `${startingCash}`;
+
 function slot1(){
-    return faces[Math.floor(Math.random()*faces.length)]
-}
-
+        return faces[Math.floor(Math.random()*faces.length)];
+};
 function slot2(){
-    return faces[Math.floor(Math.random()*faces.length)]
-}
-
+    return faces[Math.floor(Math.random()*faces.length)];
+};
 function slot3(){
-    return faces[Math.floor(Math.random()*faces.length)]
-}
+    return faces[Math.floor(Math.random()*faces.length)];
+};
 
 function spin(){
-    let number = Number(document.getElementById('gamble').value);
-    
-    if(number >= 1001 || number === '' || number <= 0 ){
-        window.alert("Your bet has to between 1 and 1000");
-        return;
-    }
-
-    spinCount++;
-    console.log(spinCount)
     document.getElementById('button').style.display = 'block';
     document.getElementById('playButton').style.display = 'none';
 
-    let intervalCount = 0;
+    let number = Number(document.getElementById('gamble').value);
+    if(number >= 1001 || number === '' || number <= 1)
+        return window.alert("Your bet has to between 1 and 1000");
 
+    if(number > startingCash)
+        return alert("not enough cash");
+
+    let intervalCount = 0;
     let interval = setInterval(function(){
         intervalCount++;
-        if(intervalCount >= 6){
-            clearInterval(interval)
-        }else{
             let slot_1 = slot1();
             document.getElementById("slot1").innerHTML = slot_1;
             let slot_2 = slot2();
@@ -41,52 +35,53 @@ function spin(){
             let slot_3 = slot3();
             document.getElementById("slot3").innerHTML = slot_3;
             console.log(slot_1, slot_2, slot_3);
-        }
-
-    }, 100)
-
-   
-    
-    const outcome = document.getElementById("outcome");
-    const money = document.getElementById("money");
-    const moneyMsg = document.getElementById("moneyMsg");
-    moneyMsg.style.display = 'none';
-
-    
-
-    if(spinCount % 5 == 0){
-        startingCash += 200;
-        console.log("You win 200")
-        moneyMsg.innerHTML = 'You won $200!';
-        moneyMsg.style.display = 'block';
-    }
-
-    if(slot_1 == slot_2 && slot_1 == slot_3){
-        let win = document.getElementById('win');
-        startingCash += number;
-        outcome.innerHTML = `You Won!`;
-        money.innerHTML = `${startingCash}`;
-        win.play();
         
-    } else {
-        startingCash -= number;
-        let lose = document.getElementById('lose');
-        outcome.innerHTML = `You Lost!`;
-        money.innerHTML = `${startingCash}`;
-        lose.play();
-    }
+         if(intervalCount >= 6){
+            clearInterval(interval);
 
-    if (startingCash <= 0){
-        outcome.innerHTML = `Game over! You're broke!`;
-        spinCount = 0;
-        return playAgain();
-    }
+            const outcome = document.getElementById("outcome");
+            const money = document.getElementById("money");
+            const moneyMsg = document.getElementById("moneyMsg");
 
+            moneyMsg.style.display = 'none';
+            spinCount++;
+            console.log(spinCount);
+        
+            if(spinCount % 5 == 0){
+                startingCash += 100;
+                console.log("You win 200")
+                moneyMsg.innerHTML = 'You won $200!';
+                moneyMsg.style.display = 'block';
+            };
+        
+            if(slot_1 == slot_2 && slot_1 == slot_3){
+                let win = document.getElementById('win');
+                startingCash += number;
+                outcome.innerHTML = `You Won!`;
+                money.innerHTML = `${startingCash}`;
+                win.play();
+                
+            } else {
+                startingCash -= number;
+                let lose = document.getElementById('lose');
+                outcome.innerHTML = `You Lost!`;
+                money.innerHTML = `${startingCash}`;
+                lose.play();
+            };
+        
+            if (startingCash <= 0){
+                outcome.innerHTML = `Game over! You're broke!`;
+                spinCount = 0;
+                return playAgain();
+            };
+        };
+    }, 200);
+    document.getElementById("money").innerHTML = `${startingCash}`;
 };
-document.getElementById("money").innerHTML = `${startingCash}`;
 
 function playAgain(){
     startingCash = 1000;
     document.getElementById('button').style.display = 'none';
     document.getElementById('playButton').style.display = 'block';
+    // document.getElementById('gamble').value = "";
 };
